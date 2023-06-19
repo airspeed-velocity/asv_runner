7f882c259ce52dd1ca3a6f26647c118933559329
Rohit Goswami <rog32@hi.is>
2023-06-19 01:59:49 +0000

    DOC: Setup basic configuration with autodoc2
    Also myst-parser

---
+++ docs/source/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'asv_runner'
+copyright = '2023, asv Developers'
+author = 'asv Developers'
+release = '0.0.4'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'autodoc2',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx_contributors',
+    'sphinx_copybutton',
+    'sphinx_design',
+    'sphinxcontrib.spelling',
+]
+
+autodoc2_render_plugin = "myst"
+autodoc2_packages = [
+    "../../asv_runner",
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'asv': ('https://asv.readthedocs.io/en/latest/', None),
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,19 @@
+# Welcome to `asv_runner`'s documentation
+
+```{toctree}
+:maxdepth: 2
+:caption: Contents
+
+apidocs/index
+```
+
+# Indices and tables
+
+- [](genindex)
+- [](modindex)
+- [](search)
+<!-- - {ref}`genindex` -->
+<!-- - {ref}`modindex` -->
+<!-- - {ref}`search` -->
+
+
