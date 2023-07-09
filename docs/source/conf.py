bfc25382e3c69b6fcf2ba11ae39748583389aada
Rohit Goswami <rog32@hi.is>
2023-07-09 16:00:37 +0000

    MAINT: Rework with black + ruff + precommit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,25 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'asv_runner'
-copyright = '2023, asv Developers'
-author = 'asv Developers'
-release = '0.0.6'
+project = "asv_runner"
+copyright = "2023, asv Developers"
+author = "asv Developers"
+release = "0.0.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
-    'autodoc2',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
-    'sphinx_contributors',
-    'sphinx_copybutton',
-    'sphinx_design',
-    'sphinxcontrib.spelling',
+    "myst_parser",
+    "autodoc2",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
+    "sphinx_contributors",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinxcontrib.spelling",
 ]
 
 autodoc2_render_plugin = "myst"
@@ -37,20 +37,19 @@ myst_enable_extensions = [
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'asv': ('https://asv.readthedocs.io/en/latest/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "asv": ("https://asv.readthedocs.io/en/latest/", None),
 }
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 
 html_theme_options = {
     "source_repository": "https://github.com/HaoZeke/asv_runner/",
