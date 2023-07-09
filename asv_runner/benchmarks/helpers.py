66eff69bfe964cdadbccc7e434bd1178760af40a
Rohit Goswami <rog32@hi.is>
2023-07-09 17:50:14 +0000

    MAINT: Add another decorator [skip_params]

--- asv_runner/benchmarks/helpers.py
+++ asv_runner/benchmarks/helpers.py
@@ -1,4 +1,37 @@
-from functools import wraps
+import functools
+
+
+def skip_params(skip_params_dict):
+    """
+    Decorator to set skip parameters for a benchmark function.
+
+    #### Parameters
+    **skip_params_dict** (`dict`):
+    A dictionary specifying the skip parameters for the benchmark function.
+    The keys represent the parameter names, and the values can be a single value
+    or a list of values.
+
+    #### Returns
+    **decorator** (`function`):
+    A decorator function that sets the skip parameters for the benchmark function.
+
+    #### Notes
+    The `skip_benchmark_for_params` decorator can be used to specify skip parameters
+    for a benchmark function. The skip parameters define combinations of values that
+    should be skipped when running the benchmark. The decorated function's `skip_params`
+    attribute will be set with the provided skip parameters, which will be used during
+    the benchmarking process.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        setattr(wrapper, "skip_params", skip_params_dict)
+        return wrapper
+
+    return decorator
 
 
 def skip_benchmark(func):
@@ -19,9 +52,13 @@ def skip_benchmark(func):
     benchmarking, it will be skipped and not included in the benchmarking
     process.
     """
-    @wraps(func)
+
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
 
     setattr(wrapper, "skip_benchmark", True)
     return wrapper
+
+
+__all__ = [skip_params, skip_benchmark]
